469b662ad83880197d72f6b1fcf1dd9f1581e9f5
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2022-12-06 11:59:36 +0100

    Es 2

--- base/js/script.js
+++ base/js/script.js
@@ -12,46 +12,105 @@
 // Bonus 1 : trasforma ogni valore di ogni animale in tutto maiuscolo tranne la prima lettera, tipo // leone => lEONE, canidi => cANIDI
 
 
-const animals = 
-[
-    {
-        name: "tigre",
-        family: "felidi",
-        classe: "mammiferi"
-    },
-    {
-        name: "leopardo",
-        family: "felidi",
-        classe: "mammiferi"
-    },
-    {
-        name: "leone",
-        family: "felidi",
-        classe: "mammiferi"
-    },
-    {
-        name: "piccione",
-        family: "volatile",
-        classe: "onnifero"
-    },
-    {
-        name: "aquila",
-        family: "volatile",
-        classe: "onnifero"
-    },
-    {
-        name: "rondine",
-        family: "volatile",
-        classe: "onnifero"
-    },
-    {
-        name: "alessio",
-        family: "umano",
-        classe: "mammiferi"
-    }
+const animals =
+    [
+        {
+            name: "tigre",
+            family: "felidi",
+            classe: "mammiferi"
+        },
+        {
+            name: "leopardo",
+            family: "felidi",
+            classe: "mammiferi"
+        },
+        {
+            name: "leone",
+            family: "felidi",
+            classe: "mammiferi"
+        },
+        {
+            name: "piccione",
+            family: "volatile",
+            classe: "onnifero"
+        },
+        {
+            name: "aquila",
+            family: "volatile",
+            classe: "onnifero"
+        },
+        {
+            name: "rondine",
+            family: "volatile",
+            classe: "onnifero"
+        },
+        {
+            name: "alessio",
+            family: "umano",
+            classe: "mammiferi"
+        }
 
-];
+    ];
 
-const mammiferi = animals.filter((animal) => animal.classe ==="mammiferi");
+const mammiferi = animals.filter((animal) => animal.classe === "mammiferi");
 
-console.log(mammiferi);
+//SALVO nella nuova varibile
+const nuoviMammiferi = mammiferi.map((mammifero) => {
+    mammifero.name = mammifero.name.charAt(0) + mammifero.name.substring(1).toUpperCase();
+
+    return mammifero;
+});
+
+console.log(nuoviMammiferi);
+
+
+console.log("Esercizio 2");
+/***
+ * 
+Crea un array di oggetti che rappresentano delle persone.
+Ogni persona ha un nome, un cognome e un'età, creiamone almeno una decina.
+
+Crea quindi un nuovo array inserendo, per ogni persona, una frase con il nome e cognome e l'indicazione se può guidare, in base all'età.
+
+ */
+
+const persone =
+    [
+        {
+            name: "Alessio",
+            surname: "Napoletano",
+            age: 24
+        },
+        {
+            name: "Sebastiano",
+            surname: "Polito",
+            age: 28
+        },
+        {
+            name: "Cristian",
+            surname: "Bevilaqua",
+            age: 16
+        },
+        {
+            name: "Axel",
+            surname: "Berti",
+            age: 18
+        },
+        {
+            name: "Gabriel",
+            surname: "Cantalicio",
+            age: 16
+        },
+    ];
+
+const maggiorenni = persone.filter((persona) => {
+    if(persona.age >=18 ){
+        return persona
+    } 
+    return false;
+
+});
+
+console.log(maggiorenni);
+
+maggiorenni.forEach((maggiorenne) => console.log(`Nome: ${maggiorenne.name} Cognome: ${maggiorenne.surname} può guidare in Italia`));
