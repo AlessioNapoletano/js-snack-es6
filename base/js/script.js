335c67c2deb84635d625ce060bdb0fca32c95e73
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2022-12-06 11:25:25 +0100

    Create project

---
+++ base/js/script.js
@@ -0,0 +1,53 @@
+// Crea un array di oggetti che rappresentano degli animali.
+// Ogni animale ha un nome, una famiglia e una classe.
+// Es:
+
+// [
+//   { nome: 'leone', famiglia: 'felidi', classe: 'mammiferi' },
+//   { nome: 'cane', famiglia: 'canidi', classe: 'mammiferi' },
+//   { nome: 'gallina', famiglia: 'fasianidi', classe: 'uccelli' },
+// ]
+
+// Crea un nuovo array con la lista dei mammiferi.
+// Bonus 1 : trasforma ogni valore di ogni animale in tutto maiuscolo tranne la prima lettera, tipo // leone => lEONE, canidi => cANIDI
+
+
+const animals = 
+[
+    {
+        name: "tigre",
+        family: "felidi",
+        classe: "mammiferi"
+    },
+    {
+        name: "leopardo",
+        family: "felidi",
+        classe: "mammiferi"
+    },
+    {
+        name: "leone",
+        family: "felidi",
+        classe: "mammiferi"
+    },
+    {
+        name: "piccione",
+        family: "volatile",
+        classe: "onnifero"
+    },
+    {
+        name: "aquila",
+        family: "volatile",
+        classe: "onnifero"
+    },
+    {
+        name: "rondine",
+        family: "volatile",
+        classe: "onnifero"
+    },
+    {
+        name: "alessio",
+        family: "umano",
+        classe: "mammiefero"
+    },
+
+]
